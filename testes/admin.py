f93d38691c5aa52b63e95e6428282551f01c7d41
Vagner <vagner.yf@gmail.com>
2015-04-30 17:55:22 -0300

    Teste Many to Many Funcionando admin.py

--- testes/admin.py
+++ testes/admin.py
@@ -3,18 +3,54 @@ from django.contrib import admin
 from django import forms
 from .models import Teste, Tag
 from categorias.models import Categoria
-from easy_select2 import select2_modelform, select2_modelform_meta, Select2TextInput
+# from django.db.models import Q
+from datetime import datetime, timedelta
+# from easy_select2 import select2_modelform, select2_modelform_meta, Select2TextInput
+
+# class TesteAdmin(admin.ModelAdmin):
+#     list_display = ('titulo','publicacao','categoria',)
+#     # fieldsets = [
+#     #     (None, {'fields': ['titulo']}),
+#     #     ('Data publicacao', {'fields': ['publicacao']}),
+#     #     ('Texto', {'fields': ['conteudo']}),
+#     #     ('Categoria',{'fields':['categoria']})
+#     #     ('Testes', {'fields':['tags', 'tags2', 'teste_art', 'relacao_teste']})
+#     #     ]
+#     list_filter = ['publicacao','categoria']
+#     search_fields = ['titulo']
+
+#     class Media:
+#       js = [
+#           '/static/grappelli/tinymce/jscripts/tiny_mce/tiny_mce.js',
+#           '/static/tinymce_setup.js',
+#       ]
+
+
+class TesteAdminForm(forms.ModelForm):
+    class Meta:
+        model = Teste
+        exclude = ['publicacao']
+
+    def __init__(self, *args, **kwargs):
+        super(TesteAdminForm, self).__init__(*args, **kwargs)
+        s = []
+        print self.instance.id, type(self.instance)
+        if self.instance.id:
+            ts = self.instance.relacao_teste
+            s = ts.all().values_list('id', flat=True)
+            s = list(s)
+        t = Teste.objects.filter(categoria='6').filter(publicacao__lte=datetime.now()).values_list('id', flat=True)[:3]
+        t = list(t)
+        t += s
+        qs = Teste.objects.filter(id__in=t)
+        # if self.instace.id:
+        #     qs = qs.exclude(id=self.instance.id)
+        self.fields['relacao_teste'].queryset = qs
 
 
 class TesteAdmin(admin.ModelAdmin):
+    form = TesteAdminForm
     list_display = ('titulo','publicacao','categoria',)
-    # fieldsets = [
-    #     (None, {'fields': ['titulo']}),
-    #     ('Data publicacao', {'fields': ['publicacao']}),
-    #     ('Texto', {'fields': ['conteudo']}),
-    #     ('Categoria',{'fields':['categoria']})
-    #     ('Testes', {'fields':['tags', 'tags2', 'teste_art', 'relacao_teste']})
-    #     ]
     list_filter = ['publicacao','categoria']
     search_fields = ['titulo']
 
--- testes/models.py
+++ testes/models.py
@@ -19,20 +19,23 @@ class Tag(models.Model):
 def limit_publicacao_choices():
     return {'publicacao__lte': datetime.now()}
 
-def filtros_q():
-    # data_hoje = datetime.now()
-    # data_antes = data_hoje - timedelta(days=190)
-    # testes_in = Teste.objects.all()[:3]
-    # filtro_q = Q(id__in=testes_in) & Q(publicacao__range=(data_antes, data_hoje)) & Q(categoria=6)
-    testes_in = Teste.objects.filter(categoria='6').filter(publicacao__lte=datetime.now()).values_list('id', flat=True)[:3]
-    # try:
-    #     testes_existente = Teste.objects.fiter(id__in='relacao_existente_id').values_list('id', flat=True)
-    #     testes_in = testes_in + testes_existente
-    # except Exception, e:
-    #     pass  
-    filtro_q = Q(id__in=testes_in)
-    return filtro_q
-
+# def filtros_q():
+#     # data_hoje = datetime.now()
+#     # data_antes = data_hoje - timedelta(days=190)
+#     # testes_in = Teste.objects.all()[:3]
+#     # filtro_q = Q(id__in=testes_in) & Q(publicacao__range=(data_antes, data_hoje)) & Q(categoria=6)
+#     testes_in = Teste.objects.filter(categoria='6').filter(publicacao__lte=datetime.now()).values_list('id', flat=True)[:3]
+#     m = [8]
+#     # testes_ex = Teste.objects.filter(id__in=m).values_list('id', flat=True)
+#     # n = 'self.relacao_teste'
+#     # testes_ex = Teste.objects.filter(id__in=n).values_list('id', flat=True)
+#     # testes_ex = list(testes_ex)
+#     testes_in = list(testes_in)
+#     # testes_in.extend(m)
+#     testes_in += m
+#     filtro_q = Q(id__in=testes_in)
+#     return filtro_q
+    
 class Teste(models.Model):
     titulo = models.CharField(max_length=100)
     conteudo = models.TextField()
@@ -43,7 +46,9 @@ class Teste(models.Model):
     # tags_2 = models.CharField(Tag, max_length=100, blank=True, null=True)
     # teste_art = models.ManyToManyField(Artigo, blank=True, null=True)
 
-    relacao_teste = models.ManyToManyField('self', blank=True, null=True, symmetrical=False, limit_choices_to=filtros_q, help_text=u'Selecione 2 artigos no máximo. ')
+    relacao_teste = models.ManyToManyField('self', blank=True, null=True, symmetrical=False)#, limit_choices_to=filtros_q, help_text=u'Selecione 2 artigos no máximo. ')
+    
+
 
     # try:
     #     relacao_existente_id = self.relacao_teste.all().values_list('id', flat=True)
